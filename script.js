631e768af8115d1f93d39e14cb3d12e5f804bef9
uohhdowuebfouwhef <112186269+uohhdowuebfouwhef@users.noreply.github.com>
2024-06-06 18:03:13 +0200

    Update script.js

--- script.js
+++ script.js
@@ -37,28 +37,28 @@ calculateFirst();
 sleep(5000).then(() => { 
 function calculate() {
 		newPos = curpos;
-		startNumber = LASTRESULT;
+		startNumber1 = LASTRESULT;
 		question = document.getElementById("fop"+ pos);
 		calcType = question.innerHTML;
 		calcType = calcType.charAt(0);
 		qNumber = operands[newPos]
 		if (calcType == "+") {
-   	 	 let result = Number(startNumber) + Number(qNumber);
+   	 	 let result = Number(startNumber1) + Number(qNumber);
 	         console.log(result);
 	         LASTRESULT = result
 	         return result;
 		} else if (calcType == "-") {
-	 		let result = Number(startNumber) - Number(qNumber);
+	 		let result = Number(startNumber1) - Number(qNumber);
 	 		console.log(result);
 	 		LASTRESULT = result
 	 		return result;
 		} else if (calcType == "*") {
-	   		let result = Number(startNumber) * Number(qNumber);
+	   		let result = Number(startNumber1) * Number(qNumber);
 	   		console.log(result);
 	   		LASTRESULT = result
 	   		return result;
 		} else if (calcType == "/") {
-		 	let result = Number(startNumber) / Number(qNumber);
+		 	let result = Number(startNumber1) / Number(qNumber);
 		 	LASTRESULT = result
 	 	 	console.log(result);
 	   	return result;
