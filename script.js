4ed18d262de4d3afb735677b1db56872ce68eebf
uohhdowuebfouwhef <112186269+uohhdowuebfouwhef@users.noreply.github.com>
2024-06-06 18:38:59 +0200

    Update script.js

--- script.js
+++ script.js
@@ -42,7 +42,7 @@ function calculate() {
 		calcType = question.innerHTML;
 		calcType = calcType.charAt(0);
 		qNumber = operands[newPos]
-	        console.log("The new calculation is " startNumber1 + calcType + qNumber)
+	        console.log("The new calculation is " tostring(startNumber1) +" " + toString(calcType)+" " + toString(qNumber))
 		if (calcType == "+") {
    	 	 let result = Number(startNumber1) + Number(qNumber);
 	         console.log(result);
